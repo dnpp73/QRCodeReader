6aeff12e4207d8c3fc95be6b49ee6d1b3696e541
Yusuke SUGAMIYA <yusuke.dnpp@gmail.com>
2020-11-06 03:10:53 +0900

    add Package.swift

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "QRCodeReader",
+    platforms: [
+        .iOS(.v10),
+    ],
+    products: [
+        .library(name: "QRCodeReader", targets: ["QRCodeReader"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/dnpp73/GPUCIImageView", .upToNextMinor(from: "0.1.0")),
+        .package(url: "https://github.com/dnpp73/SimpleCamera", .upToNextMinor(from: "0.1.1")),
+    ],
+    targets: [
+        .target(
+            name: "QRCodeReader",
+            dependencies: [],
+            path: "Sources"
+        ),
+    ]
+)
